bff8af689a01173f0ae3faf2473b0af4b41af2a0
ogaip <ogaip87@gmail.com>
2024-07-10 15:55:44 -0400

    primera clase del curso de JavaScript - MouroDev

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,8 @@
+// Esto es un comentario simple
+
+console.log("Hello World!")
+console.log('Hello World!')
+console.log(`hello world!`)
+
+// console.log("Hello World!")
+
---
+++ Basic/01-variables.js
@@ -0,0 +1,25 @@
+// var
+
+var helloWorld = "Hello, JavaScript!"
+console.log(helloWorld)
+
+helloWorld= "Hola de nuevo, JavaSCript!"
+
+console.log(helloWorld)
+
+// let
+
+let helloWorld2 = "Hello, JavaScript 2!"
+console.log(helloWorld2)
+
+helloWorld2= "Hola de nuevo, JavaSCript 2!"
+console.log(helloWorld2)
+
+//const
+
+const helloWorld3 = "Hello, JavaScript 3!"
+console.log(helloWorld3)
+
+helloWorld= "Hola de nuevo, JavaSCript!"
+
+console.log(helloWorld)
